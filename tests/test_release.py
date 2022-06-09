7b3f2c071cc049590f5331932e6fb659cf4dc13e
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-06-09 13:14:47 +0200

    refactor: make test structure more generic

--- tests/test_release.py
+++ tests/test_release.py
@@ -2,9 +2,13 @@ import pytest
 from kwalitee import Release
 import json
 
-def test_tag():
+def get_json_data(array_pos):
     f = open('tests/assets/ocrd_all_releases.json')
     data = json.load(f)
-    first_release = data[0]
+    return data[array_pos]
+    
+
+def test_tag():
+    first_release = get_json_data(0)
     release = Release(first_release)
     assert release.tag == "v2022-06-03"
