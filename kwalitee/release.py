04ecfc1865ab04ce202675e27d5a9a56798d5820
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-06-09 13:32:44 +0200

    feat: add projects that are part of a release

--- kwalitee/release.py
+++ kwalitee/release.py
@@ -1,16 +1,19 @@
-import json
+import json, re
 from ocrd_utils import getLogger
 
 class Release():
 
     def __init__(self, json_data):
         self.tag = json_data['tag_name']
+        self.projects = self.get_projects(json_data)
 
     def __str__(self):
         pass
 
-    def get_projects(self):
-        pass
+    def get_projects(self, json_data):
+        markdown_desc = json_data['body']
+        pattern = r'### \[(.*?)\]'
+        return re.findall(pattern, markdown_desc)
 
     def to_json(self):
         pass
--- tests/test_release.py
+++ tests/test_release.py
@@ -11,4 +11,9 @@ def get_json_data(array_pos):
 def test_tag():
     first_release = get_json_data(0)
     release = Release(first_release)
-    assert release.tag == "v2022-06-03"
+    assert release.tag == 'v2022-06-03'
+
+def test_released_projects():
+    first_release = get_json_data(0)
+    release = Release(first_release)
+    assert release.projects == ['core', 'ocrd_detectron2', 'ocrd_keraslm', 'ocrd_olena', 'ocrd_segment', 'opencv-python', 'workflow-configuration']
