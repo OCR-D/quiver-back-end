668228ea030dbd46e54910e2f506aefef597f3a1
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-06-20 08:15:19 +0200

    docs: add description of the filter

--- kwalitee/filter.py
+++ kwalitee/filter.py
@@ -1,3 +1,14 @@
+"""
+In our resulting `ocrd_all_release.json` each project (= submodule of ocrd_all)
+should appear only once – listed under the most recent ocrd_all release it is part of.
+
+Therefore we filter all Release objects after their creation to determine if their projects
+are mentioned for the first time (making the current release the most recent one) or if 
+they have already been mentioned (which means that a more recent ocrd_all release also
+contains the project). A project is kept in the first case while it is tossed in the latter.
+
+If a Release object has an empty projects attribute, it is tossed completely.
+"""
 def filter_release_projects(releases):
     released_projects = []
     filtered_releases = []
@@ -17,4 +28,3 @@ def filter_release_projects(releases):
             filtered_releases.append(new_release_object)
 
     return filtered_releases
-    
