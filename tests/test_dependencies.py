e7ebb00e52acaaa5261c3cd5f7447436fc16cd4c
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-06-27 08:34:58 +0200

    fix: core deps list

---
+++ tests/test_dependencies.py
@@ -0,0 +1,40 @@
+import re
+
+def test_core_deps():
+    with open('core_deps.txt', 'r') as file:
+        expected = ['atomicwrites==1.4.0',
+                    'attrs==21.4.0',
+                    'bagit==1.8.1',
+                    'bagit-profile==1.3.1',
+                    'certifi==2022.6.15',
+                    'charset-normalizer==2.0.12',
+                    'click==8.1.3',
+                    'Deprecated==1.2.0',
+                    'Flask==2.1.2',
+                    'idna==3.3',
+                    'importlib-metadata==4.11.4',
+                    'importlib-resources==5.8.0',
+                    'itsdangerous==2.1.2',
+                    'Jinja2==3.1.2',
+                    'jsonschema==4.6.0',
+                    'lxml==4.9.0',
+                    'MarkupSafe==2.1.1',
+                    'numpy==1.21.6',
+                    'ocrd==2.35.0',
+                    'ocrd-modelfactory==2.35.0',
+                    'ocrd-models==2.35.0',
+                    'ocrd-utils==2.35.0',
+                    'ocrd-validators==2.35.0',
+                    'opencv-python-headless==4.6.0.66',
+                    'Pillow==9.1.1',
+                    'pyrsistent==0.18.1',
+                    'PyYAML==6.0',
+                    'requests==2.28.0',
+                    'Shapely==1.8.2',
+                    'typing_extensions==4.2.0',
+                    'urllib3==1.26.9',
+                    'Werkzeug==2.1.2',
+                    'wrapt==1.14.1',
+                    'zipp==3.8.0']
+        result = file.read().splitlines()
+        assert result == expected
