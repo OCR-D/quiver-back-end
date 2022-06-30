07f4f463a3f7144d46fec7d0be90d48a59283f26
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-06-30 15:04:41 +0200

    chore: update deps files

--- dependencies.sh
+++ dependencies.sh
@@ -40,46 +40,45 @@ echo '[' >> $CURRENT_DIR'/deps.json'
 
 for NAME in $SUBMODULE_NAMES
 do
-    DIR='/submodules/ocrd_all/'$NAME
-    echo "Currently processing " $NAME " ..."
-    cd $CURRENT_DIR$DIR || exit
-    python -m venv venv
-    VENV=$PWD'/venv/bin/python'
+    if [ $NAME != 'tesseract' ] && [ $NAME != 'opencv-python' ]; then
+        DIR='/submodules/ocrd_all/'$NAME
+        echo "Currently processing " $NAME " ..."
+        cd $CURRENT_DIR$DIR || exit
+        python -m venv venv
+        VENV=$PWD'/venv/bin/python'
 
-    # install pkgs
-    if [ -f 'requirements.txt' ]; then
-        CMD_INSTALL=$(echo '-m pip install -r requirements.txt .')
-        $VENV $CMD_INSTALL
-    elif [ -f 'requirements_test.txt' ]; then
-        CMD_INSTALL=$(echo '-m pip install -r requirements_test.txt .')
-        $VENV $CMD_INSTALL
-    elif [ -f 'setup.py' ]; then
-        CMD_INSTALL=$(echo '-m pip install .')
-        $VENV $CMD_INSTALL
-    fi
+        # install pkgs
+        if [ -f 'requirements.txt' ]; then
+            CMD_INSTALL=$(echo '-m pip install -r requirements.txt .')
+            $VENV $CMD_INSTALL
+        elif [ -f 'setup.py' ]; then
+            CMD_INSTALL=$(echo '-m pip install .')
+            $VENV $CMD_INSTALL
+        fi
 
-    # get deps and save them to JSON
-    CMD_FREZE=$(echo '-m pip freeze -l')
-    echo '{"'$NAME'": [' >> $CURRENT_DIR'/deps.json'
-    RESULT=$($VENV $CMD_FREZE)
-    for RES in $RESULT
-    do
-        # check if dependency also occurs in core_deps.txt
-        IS_CORE_DEP="false"
-        for LINE in $(cat $CURRENT_DIR/core_deps.txt)
+        # get deps and save them to JSON
+        CMD_FREZE=$(echo '-m pip freeze -l')
+        echo '{"'$NAME'": [' >> $CURRENT_DIR'/deps.json'
+        RESULT=$($VENV $CMD_FREZE)
+        for RES in $RESULT
         do
-            if [ $RES == $LINE ]; then
-                IS_CORE_DEP="true"
+            # check if dependency also occurs in core_deps.txt
+            IS_CORE_DEP="false"
+            for LINE in $(cat $CURRENT_DIR/core_deps.txt)
+            do
+                if [ $RES == $LINE ]; then
+                    IS_CORE_DEP="true"
+                fi
+            done
+
+            if [ $IS_CORE_DEP == "false" ]; then
+                PKG=$(echo $RES | cut -d'=' -f1)
+                VER=$(echo $RES | cut -d'=' -f3)
+                echo -n '{"'$PKG'": "'$VER'"},' >> $CURRENT_DIR'/deps.json'
             fi
         done
-
-        if [ $IS_CORE_DEP == "false" ]; then
-            PKG=$(echo $RES | cut -d'=' -f1)
-            VER=$(echo $RES | cut -d'=' -f3)
-            echo -n '{"'$PKG'": "'$VER'"},' >> $CURRENT_DIR'/deps.json'
-        fi
-    done
-    echo -n ']},' >> $CURRENT_DIR'/deps.json'
+        echo -n ']},' >> $CURRENT_DIR'/deps.json'
+    fi
 done
 
 echo ']' >> $CURRENT_DIR'/deps.json'
