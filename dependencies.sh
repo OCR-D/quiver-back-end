b78e0d32e5205e925a548da7f8dcb581453d47e3
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-06-30 12:49:03 +0200

    chore: remove comments

--- dependencies.sh
+++ dependencies.sh
@@ -14,78 +14,77 @@ SUBMODULE_NAMES=$(
 
 CURRENT_DIR=$PWD
 
-if [ $1 == "update" ]; then
-    echo "Update ocrd_all and submodules."
-    #cd submodules/ocrd_all || exit
-    git submodule update submodules/ocrd_all
+#if [ $1 == "update" ]; then
+#    echo "Update ocrd_all and submodules."
+#    #cd submodules/ocrd_all || exit
+#    git submodule update submodules/ocrd_all
+#
+#    # we save the output of the submodule update and then check, if something in the requirements/
+#    # dependencies has changed. we need this information to update our deps.json later.
+#    #git submodule foreach --quiet 'bash ../../../submodule_update.sh $name $CURRENT_DIR'
+#    cd $CURRENT_DIR || exit
+#fi
 
-    # we save the output of the submodule update and then check, if something in the requirements/
-    # dependencies has changed. we need this information to update our deps.json later.
-    #git submodule foreach --quiet 'bash ../../../submodule_update.sh $name $CURRENT_DIR'
-    cd $CURRENT_DIR || exit
-fi
 
+# get the main OCR-D dependencies which stem from core and that most processors have in common
+cd 'submodules/ocrd_all/core' || exit
+python -m venv venv
+VENV=$PWD'/venv/bin/python'
+CMD='-m pip install ocrd'
+$VENV $CMD
+CMD='-m pip freeze -l'
+$VENV $CMD > $CURRENT_DIR'/core_deps.txt'
+cd $CURRENT_DIR || exit
 
-## get the main OCR-D dependencies which stem from core and that most processors have in common
-#cd 'submodules/ocrd_all/core' || exit
-#python -m venv venv
-#VENV=$PWD'/venv/bin/python'
-#CMD='-m pip install ocrd'
-#$VENV $CMD
-#CMD='-m pip freeze -l'
-#$VENV $CMD > $CURRENT_DIR'/core_deps.txt'
-#cd $CURRENT_DIR || exit
-#
-#echo '[' >> $CURRENT_DIR'/deps.json'
-#
-#for NAME in $SUBMODULE_NAMES
-#do
-#    DIR='/submodules/ocrd_all/'$NAME
-#    echo "Currently processing " $NAME " ..."
-#    cd $CURRENT_DIR$DIR || exit
-#    python -m venv venv
-#    VENV=$PWD'/venv/bin/python'
-#
-#    # install pkgs
-#    if [ -f 'requirements.txt' ]; then
-#        CMD_INSTALL=$(echo '-m pip install -r requirements.txt .')
-#        $VENV $CMD_INSTALL
-#    elif [ -f 'requirements_test.txt' ]; then
-#        CMD_INSTALL=$(echo '-m pip install -r requirements_test.txt .')
-#        $VENV $CMD_INSTALL
-#    elif [ -f 'setup.py' ]; then
-#        CMD_INSTALL=$(echo '-m pip install .')
-#        $VENV $CMD_INSTALL
-#    fi
-#
-#    # get deps and save them to JSON
-#    CMD_FREZE=$(echo '-m pip freeze -l')
-#    echo '{"'$NAME'": [' >> $CURRENT_DIR'/deps.json'
-#    RESULT=$($VENV $CMD_FREZE)
-#    for RES in $RESULT
-#    do
-#        # check if dependency also occurs in core_deps.txt
-#        IS_CORE_DEP="false"
-#        for LINE in $(cat $CURRENT_DIR/core_deps.txt)
-#        do
-#            if [ $RES == $LINE ]; then
-#                IS_CORE_DEP="true"
-#            fi
-#        done
-#
-#        if [ $IS_CORE_DEP == "false" ]; then
-#            PKG=$(echo $RES | cut -d'=' -f1)
-#            VER=$(echo $RES | cut -d'=' -f3)
-#            echo -n '{"'$PKG'": "'$VER'"},' >> $CURRENT_DIR'/deps.json'
-#        fi
-#    done
-#    echo -n ']},' >> $CURRENT_DIR'/deps.json'
-#done
-#
-#echo ']' >> $CURRENT_DIR'/deps.json'
-#
-#cd $CURRENT_DIR || exit
-#
-## make JSON valid
-#sed -i 's/},]/}]/g' deps.json
-#
+echo '[' >> $CURRENT_DIR'/deps.json'
+
+for NAME in $SUBMODULE_NAMES
+do
+    DIR='/submodules/ocrd_all/'$NAME
+    echo "Currently processing " $NAME " ..."
+    cd $CURRENT_DIR$DIR || exit
+    python -m venv venv
+    VENV=$PWD'/venv/bin/python'
+
+    # install pkgs
+    if [ -f 'requirements.txt' ]; then
+        CMD_INSTALL=$(echo '-m pip install -r requirements.txt .')
+        $VENV $CMD_INSTALL
+    elif [ -f 'requirements_test.txt' ]; then
+        CMD_INSTALL=$(echo '-m pip install -r requirements_test.txt .')
+        $VENV $CMD_INSTALL
+    elif [ -f 'setup.py' ]; then
+        CMD_INSTALL=$(echo '-m pip install .')
+        $VENV $CMD_INSTALL
+    fi
+
+    # get deps and save them to JSON
+    CMD_FREZE=$(echo '-m pip freeze -l')
+    echo '{"'$NAME'": [' >> $CURRENT_DIR'/deps.json'
+    RESULT=$($VENV $CMD_FREZE)
+    for RES in $RESULT
+    do
+        # check if dependency also occurs in core_deps.txt
+        IS_CORE_DEP="false"
+        for LINE in $(cat $CURRENT_DIR/core_deps.txt)
+        do
+            if [ $RES == $LINE ]; then
+                IS_CORE_DEP="true"
+            fi
+        done
+
+        if [ $IS_CORE_DEP == "false" ]; then
+            PKG=$(echo $RES | cut -d'=' -f1)
+            VER=$(echo $RES | cut -d'=' -f3)
+            echo -n '{"'$PKG'": "'$VER'"},' >> $CURRENT_DIR'/deps.json'
+        fi
+    done
+    echo -n ']},' >> $CURRENT_DIR'/deps.json'
+done
+
+echo ']' >> $CURRENT_DIR'/deps.json'
+
+cd $CURRENT_DIR || exit
+
+# make JSON valid
+sed -i 's/},]/}]/g' deps.json
