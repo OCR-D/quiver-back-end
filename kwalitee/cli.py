fc88f7639c8c95f9cf66640de16ca84e2a6aaf77
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-07-01 13:28:01 +0200

    feat: create list of possibly conflicting dependencies

--- kwalitee/cli.py
+++ kwalitee/cli.py
@@ -100,6 +100,39 @@ def generate_ocrd_all_releases(output=None):
         print(json_str)
 
 
+@cli.command('dep-conflicts', help='''
+
+    Generate JSON for possibly conflicting depencies. The output is written to 'dep_conflicts.json'.
+
+''')
+def generate_dependency_conflicts():
+    '''
+    Generate JSON for possibly conflicting depencies
+    '''
+    # revert dependencies
+    f = open('deps.json')
+    deps_json = json.load(f)
+
+    result = {}
+    for dependency in deps_json:
+        deps = deps_json[dependency]
+        for pkg, version in deps.items():
+            if not pkg in result:
+                result[pkg] = {}
+                result[pkg][version] = dependency
+            else:
+                result[pkg][version] = dependency
+
+    # toss every dependency that only has one version.
+    # it'll never have any conflicts because a) only one project uses it or b) several projects use the same version.
+    filtered = {}
+    for pkg in result:
+        if not len(result[pkg]) == 1:
+            filtered[pkg] = result[pkg]
+    json_str = json.dumps(filtered, indent=4, sort_keys=True)
+    Path('dep_conflicts.json').write_text(json_str, encoding='utf-8')
+
+
 @cli.command('ocrd-tool')
 @click.option('-o', '--output', help="Output file. Omit to print to STDOUT")
 @pass_ctx
