50ebf867225189369f70b49c4f2b96cd84284128
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-07-08 15:50:53 +0200

    refactor: remove cl arguments

--- dependencies.sh
+++ dependencies.sh
@@ -14,88 +14,73 @@ SUBMODULE_NAMES=$(
 
 CURRENT_DIR=$PWD
 
-if [ $1 == 'init' ]; then
-    # get the main OCR-D dependencies which stem from core and that most processors have in common.
-    cd 'submodules/ocrd_all/core' || exit
-    python -m venv venv
-    VENV=$PWD'/venv/bin/python'
-    CMD='-m pip install ocrd'
-    $VENV $CMD
-    CMD='-m pip freeze -l'
-    $VENV $CMD > $CURRENT_DIR'/core_deps.txt'
-    cd $CURRENT_DIR || exit
+# get the main OCR-D dependencies which stem from core and that most processors have in common.
+cd 'submodules/ocrd_all/core' || exit
+python -m venv venv
+VENV=$PWD'/venv/bin/python'
+CMD='-m pip install ocrd'
+$VENV $CMD
+CMD='-m pip freeze -l'
+$VENV $CMD > $CURRENT_DIR'/core_deps.txt'
+cd $CURRENT_DIR || exit
 
-    echo '{' >> $CURRENT_DIR'/deps.json'
+echo '{' >> $CURRENT_DIR'/deps.json'
 
-    for NAME in $SUBMODULE_NAMES
-    do
-        if [ $NAME != 'tesseract' ] && [ $NAME != 'opencv-python' ]; then
-            DIR='/submodules/ocrd_all/'$NAME
-            echo "Currently processing " $NAME " ..."
-            cd $CURRENT_DIR$DIR || exit
-            python -m venv venv
-            VENV=$PWD'/venv/bin/python'
+for NAME in $SUBMODULE_NAMES
+do
+    if [ $NAME != 'tesseract' ] && [ $NAME != 'opencv-python' ]; then
+        DIR='/submodules/ocrd_all/'$NAME
+        echo "Currently processing " $NAME " ..."
+        cd $CURRENT_DIR$DIR || exit
+        python -m venv venv
+        VENV=$PWD'/venv/bin/python'
 
-            # install pkgs
-            if [ -f 'requirements.txt' ]; then
-                CMD_INSTALL=$(echo '-m pip install -r requirements.txt .')
-                $VENV $CMD_INSTALL
-            elif [ -f 'setup.py' ]; then
-                CMD_INSTALL=$(echo '-m pip install .')
-                $VENV $CMD_INSTALL
-            fi
+        # install pkgs
+        if [ -f 'requirements.txt' ]; then
+            CMD_INSTALL=$(echo '-m pip install -r requirements.txt .')
+            $VENV $CMD_INSTALL
+        elif [ -f 'setup.py' ]; then
+            CMD_INSTALL=$(echo '-m pip install .')
+            $VENV $CMD_INSTALL
+        fi
 
-            # get deps and save them to JSON
-            CMD_FREZE=$(echo '-m pip freeze -l')
-            echo '"'$NAME'": {' >> $CURRENT_DIR'/deps.json'
-            RESULT=$($VENV $CMD_FREZE)
-            for RES in $RESULT
+        # get deps and save them to JSON
+        CMD_FREZE=$(echo '-m pip freeze -l')
+        echo '"'$NAME'": {' >> $CURRENT_DIR'/deps.json'
+        RESULT=$($VENV $CMD_FREZE)
+        for RES in $RESULT
+        do
+            # check if dependency also occurs in core_deps.txt
+            IS_CORE_DEP="false"
+            for LINE in $(cat $CURRENT_DIR/core_deps.txt)
             do
-                # check if dependency also occurs in core_deps.txt
-                IS_CORE_DEP="false"
-                for LINE in $(cat $CURRENT_DIR/core_deps.txt)
-                do
-                    if [ $RES == $LINE ]; then
-                        IS_CORE_DEP="true"
-                    fi
-                done
-
-                if [ $IS_CORE_DEP == "false" ]; then
-                    PKG=$(echo $RES | cut -d'=' -f1)
-                    VER=$(echo $RES | cut -d'=' -f3)
-
-                    # in each venv the project itself is also installed.
-                    # since it's not a dependency, we omit it.
-                    PKG_NORMALIZED=$(echo "$PKG" | sed -e 's/-/_/g')
-                    STARTS_WITH_FILE=$(echo "$PKG" | grep -E '^file')
-                    IS_AT=$(echo "$PKG" | grep -E '@')
-                    if [ $PKG_NORMALIZED != $NAME ] && [ ! $IS_AT ] && [ ! $STARTS_WITH_FILE ]; then
-                        echo -n '"'$PKG'": "'$VER'",' >> $CURRENT_DIR'/deps.json'
-                    fi
+                if [ $RES == $LINE ]; then
+                    IS_CORE_DEP="true"
                 fi
             done
-            echo -n '},' >> $CURRENT_DIR'/deps.json'
-        fi
-    done
 
-    echo '}' >> $CURRENT_DIR'/deps.json'
+            if [ $IS_CORE_DEP == "false" ]; then
+                PKG=$(echo $RES | cut -d'=' -f1)
+                VER=$(echo $RES | cut -d'=' -f3)
 
-    cd $CURRENT_DIR || exit
+                # in each venv the project itself is also installed.
+                # since it's not a dependency, we omit it.
+                PKG_NORMALIZED=$(echo "$PKG" | sed -e 's/-/_/g')
+                STARTS_WITH_FILE=$(echo "$PKG" | grep -E '^file')
+                IS_AT=$(echo "$PKG" | grep -E '@')
+                if [ $PKG_NORMALIZED != $NAME ] && [ ! $IS_AT ] && [ ! $STARTS_WITH_FILE ]; then
+                    echo -n '"'$PKG'": "'$VER'",' >> $CURRENT_DIR'/deps.json'
+                fi
+            fi
+        done
+        echo -n '},' >> $CURRENT_DIR'/deps.json'
+    fi
+done
 
-    # make JSON valid
-    sed -i 's/},}/}}/g' deps.json
-    sed -i 's/,}/}/g' deps.json
-fi
+echo '}' >> $CURRENT_DIR'/deps.json'
 
+cd $CURRENT_DIR || exit
 
-# TODO
-if [ $1 == "update" ]; then
-    echo "Update ocrd_all and submodules."
-    #cd submodules/ocrd_all || exit
-    git submodule update submodules/ocrd_all
-
-    # we save the output of the submodule update and then check, if something in the requirements/
-    # dependencies has changed. we need this information to update our deps.json later.
-    #git submodule foreach --quiet 'bash ../../../submodule_update.sh $name $CURRENT_DIR'
-    cd $CURRENT_DIR || exit
-fi
+# make JSON valid
+sed -i 's/},}/}}/g' deps.json
+sed -i 's/,}/}/g' deps.json
