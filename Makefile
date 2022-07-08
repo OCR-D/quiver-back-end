492469b01967ff5b51c6a57c7a26a2e8e0ea875a
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-07-08 15:38:31 +0200

    fix: initializing the submodules + dep creation fix

--- Makefile
+++ Makefile
@@ -1,17 +1,13 @@
 dependencies:
-	if [ -f 'deps.json' ]; then\
-		$(MAKE) update-deps;\
-	else\
-		$(MAKE) init-deps;\
-	fi
-
-init-deps:
+	rm deps.json
+	rm dep_conflicts.json
 	bash dependencies.sh init
-	ocrd-kwalitee dep-conflicts
-
-update-deps:
-	bash dependencies.sh update
+	python3 kwalitee/dependencies.py
 
 .PHONY: repos.json
 repos.json: dependencies
 	ocrd-kwalitee json > "$@"
+
+init:
+	git submodule update --init
+	git submodule foreach --recursive 'git submodule update --init'
--- kwalitee/cli.py
+++ kwalitee/cli.py
@@ -100,45 +100,6 @@ def generate_ocrd_all_releases(output=None):
     else:
         print(json_str)
 
-
-@cli.command('dep-conflicts', help='''
-
-    Generate JSON for possibly conflicting depencies. The output is written to 'dep_conflicts.json'.
-
-''')
-def generate_dependency_conflicts():
-    '''
-    Generate JSON for possibly conflicting depencies
-    '''
-    
-    # revert dependencies
-    f = open('deps.json')
-    deps_json = json.load(f)
-
-    result = {}
-    for dependency in deps_json:
-        deps = deps_json[dependency]
-        for pkg, version in deps.items():
-            if not pkg in result:
-                result[pkg] = {}
-                result[pkg][dependency] = version
-            else:
-                result[pkg][dependency] = version
-
-    # toss every dependency that only has one version.
-    # it'll never have any conflicts because a) only one project uses it or b) several projects use the same version.
-    filtered = {}
-    for pkg in result:
-        versions = result[pkg].values()
-        versions_wo_duplicates = list(set(versions))
-        if not len(result[pkg]) == 1 and not len(versions_wo_duplicates) == 1:
-            filtered[pkg] = result[pkg]
-    json_str = json.dumps(filtered, indent=4, sort_keys=True)
-    # remove old version of dep_conflicts.json
-    remove('dep_conflicts.json')
-    Path('dep_conflicts.json').write_text(json_str, encoding='utf-8')
-
-
 @cli.command('ocrd-tool')
 @click.option('-o', '--output', help="Output file. Omit to print to STDOUT")
 @pass_ctx
