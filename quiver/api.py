bad982d74a171e2f8c1261faef78351ba793c22b
Michelle Weidling <weidling@sub.uni-goettingen.de>
2022-11-06 14:27:31 +0100

    feat: add first draft of POST end point

--- quiver/api.py
+++ quiver/api.py
@@ -1,15 +1,20 @@
-from typing import Union
-
 from fastapi import FastAPI
+import json
+from typing import Union
+from typing import Dict
+from pathlib import Path
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/nextflow/")
+def save_workflow(item: Dict[str, Union[str, float,Dict]]):
+    filtered = filter_result(item)
+    json_str = json.dumps(filtered, indent=4, sort_keys=True)
+    event = filtered['event']
+    output_name = filtered['runName'] + filtered['runId']
+    output = 'workflows/results/' + output_name + '_' + event + '.json'
+    Path(output).write_text(json_str, encoding='utf-8')
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def filter_result(item: Dict[str, Union[str, float,Dict]]):
+    return item
