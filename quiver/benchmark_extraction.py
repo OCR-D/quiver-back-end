2971c234b20f968685792007c141d6d99777c627
mweidling <13831557+mweidling@users.noreply.github.com>
2022-12-10 20:33:09 +0100

    Add missing metadata (#46) and improve bash script
    * feat: separate OCR and eval workflows
    
    * feat: add wf for selected pages
    
    * update evaluation results
    
    * feat: add wf for slower processors
    
    * fix: consider multiple mets:agents with same attr
    
    * feat: extract METADATA.yml
    
    * feat: update quiver
    
    * test: add tests
    
    * feat: add missing metadata
    
    * fix: GT label
    
    * fix: merge issues
    
    * feat: ad page numbers and tests
    
    * fix: use tesserocr-segment without param (causes an error for unknown reasons)

--- quiver/benchmark_extraction.py
+++ quiver/benchmark_extraction.py
@@ -2,26 +2,16 @@
 benchmarking. It extracts the relevant information from the NextFlow processes. """
 
 import json
+import re
 import sys
 import xml.etree.ElementTree as ET
 from os import listdir, scandir
-import re
 from typing import Any, Dict, List, Union
 
+import yaml
 from .constants import *
 
-#{
- #   "metadata": {
- #     "eval_workflow_url": "https://example.org/workflow/eval1",
- #     "eval_data": "https://example.org/workspace/345",
- #     "data_properties": {
- #       "fonts": ["antiqua", "fraktur"],
- #       "publication_year": "19. century",
- #       "number_of_pages": "100",
- #       "layout": "simple"
- #     }
- #   }
- # }
+METS = '{http://www.loc.gov/METS/}'
 
 def make_result_json(workspace_path: str, mets_path: str) -> Dict[str, Union[str, Dict]]:
     data_name = get_workspace_name(workspace_path)
@@ -33,7 +23,7 @@ def make_result_json(workspace_path: str, mets_path: str) -> Dict[str, Union[str
     }
 
 def get_workspace_name(workspace_path: str) -> str:
-    return workspace_path.split('/')[-2]
+    return workspace_path.split('/')[-1]
 
 def make_metadata(workspace_path: str, mets_path: str) -> Dict[str, Union[str, Dict]]:
     return {
@@ -44,7 +34,7 @@ def make_metadata(workspace_path: str, mets_path: str) -> Dict[str, Union[str, D
             'eval_workspace': get_workspace(workspace_path, 'evaluation'),
             'workflow_steps': get_workflow_steps(mets_path),
             'workflow_model': get_workflow_model(mets_path),
-            'document_metadata': ''
+            'document_metadata': get_document_metadata(workspace_path)
         }
 
 def get_workflow(workspace_path: str, wf_type: str) -> Dict[str, str]:
@@ -90,15 +80,25 @@ def get_workflow_steps(mets_path: str) -> List[str]:
     name_elements = get_element_from_mets(mets_path, xpath)
     formatted_names = []
     for e in name_elements:
-        formatted_names.append(e.text.split(' ')[0])
+        name = e.text.split(' ')[0]
+        if 'ocrd-dinglehopper' not in name:
+            formatted_names.append(name)
 
     return formatted_names
 
 def get_workflow_model(mets_path: str) -> str:
-    xpath = f'.//{METS}agent[@OTHERROLE="layout/segmentation/region"]/{METS}note[@{OCRD}option="parameter"]'
-    parameters = get_element_from_mets(mets_path, xpath)[0].text
-    params_json = json.loads(parameters)
-    return params_json['model']
+    OCRD = '{https://ocr-d.de}'
+    try:
+        xpath = f'.//{METS}agent[@OTHERROLE="recognition/text-recognition"]/{METS}note[@{OCRD}option="parameter"]'
+        parameters = get_element_from_mets(mets_path, xpath)[0].text
+        params_json = json.loads(parameters)
+        return params_json['checkpoint_dir']
+    except:
+        xpath = f'.//{METS}agent[@OTHERROLE="layout/segmentation/region"]/{METS}note[@{OCRD}option="parameter"]'
+        parameters = get_element_from_mets(mets_path, xpath)[-1].text
+        params_json = json.loads(parameters)
+        return params_json['model']
+
 
 def get_eval_tool(mets_path: str) -> str:
     xpath = f'.//{METS}agent[@OTHERROLE="recognition/text-recognition"]/{METS}name'
@@ -106,12 +106,58 @@ def get_eval_tool(mets_path: str) -> str:
 
 def get_gt_workspace(workspace_path: str) -> Dict[str, str]:
     current_workspace = get_workspace_name(workspace_path)
-    url = f'{QUIVER_MAIN}/{current_workspace}.ocrd.zip'
-    label = 'TODO'
+    split_workspace_name = current_workspace.split('_')
+    font = ''
+    if split_workspace_name[1] == 'ant':
+        font = 'Antiqua'
+    elif split_workspace_name[1] == 'frak':
+        font = 'Black letter'
+    else:
+        font = 'Font Mix'
+    url = 'https://github.com/OCR-D/quiver-data/blob/main/' + current_workspace + '.ocrd.zip'
+    label = f'GT workspace {split_workspace_name[0]}th century {font} {split_workspace_name[2]} layout'
     return {
         '@id': url,
         'label': label
     }
+
+def get_document_metadata(workspace_path: str) -> Dict[str, Dict[str, str]]:
+    result = {
+        'eval_workflow_url': 'https://github.com/OCR-D/quiver-back-end/tree/main/workflows/ocrd_workflows/dinglehopper.txt',
+        'eval_data': 'https://github.com/OCR-D/quiver-back-end/TODO',
+        'data_properties': {
+            'fonts': '',
+            'publication_year': '',
+            'number_of_pages': get_no_of_pages(workspace_path),
+            'layout': ''
+        }
+    }
+    with open(workspace_path + '/METADATA.yml', 'r', encoding='utf-8') as file:
+        metadata = yaml.safe_load(file)
+        scripts = metadata['script']
+        fonts = []
+        for script in scripts:
+            if script == 'Latn':
+                fonts.append('Antiqua')
+            if script == 'Goth':
+                fonts.append('Black Letter')
+            if script == 'Hebr':
+                fonts.append('Hebrew')
+            if script == 'Grek':
+                fonts.append('Ancient Greek')
+        result['data_properties']['fonts'] = fonts
+
+        earliest_publication_year = metadata['time']['notBefore']
+        publication_century = int(earliest_publication_year[:2]) + 1
+        result['data_properties']['publication_year'] = f'{publication_century}th century'
+
+        result['data_properties']['layout'] = metadata['title'].split('_')[-1]
+    return result
+
+def get_no_of_pages(workspace_path: str) -> int:
+    img_path = workspace_path + '/OCR-D-IMG'
+    return len(listdir(img_path))
+
 
 def extract_benchmarks(workspace_path: str, mets_path: str) -> Dict[str, Dict[str, Any]]:
     json_dirs = get_eval_jsons(workspace_path)
--- workflows/execute_workflows.sh
+++ workflows/execute_workflows.sh
@@ -27,9 +27,11 @@ rename_and_move_nextflow_result() {
 run() {
     # $1: $WORKFLOW
     # $2: $DIR_NAME
+    # $3: $WS_DIR
     adjust_workflow_settings "$1" "$2"
     nextflow run "$1" -with-weblog http://127.0.0.1:8000/nextflow/
     rename_and_move_nextflow_result "$1" "$2"
+    save_workspaces "$3" "$2" "$1"
 }
 
 save_workspaces() {
@@ -38,7 +40,7 @@ save_workspaces() {
     # $3: $WORKFLOW
     echo "Zipping workspace $1"
     ocrd zip bag -d $1 -i $1 $1
-    WORKFLOW_NAME=$(basename -s .txt.nf "$1")
+    WORKFLOW_NAME=$(basename -s .txt.nf "$3")
     mv "$WORKSPACE_DIR"/"$2".zip "$WORKFLOW_DIR"/results/"$2"_"$WORKFLOW_NAME".zip
 }
 
@@ -48,7 +50,6 @@ echo "Initialize submodules …"
 git -C submodules/quiver-data submodule update --init
 git -C submodules/oton submodule update --init
 
-# update the data from quiver-data repository if necessary
 echo "Update quiver-data if necessary …"
 git -C submodules/quiver-data pull origin main
 
@@ -62,15 +63,14 @@ echo "Installing quiver-ocrd"
 pip install .
 echo "quiver-ocrd installation done."
 
-quiver-ocrd || { echo "quiver-ocrd not installed"; exit 1 }
-oton || { echo "quiver-ocrd not installed"; exit 1 }
-
-echo "Restore OCR-D workspaces from BagIts …"
-for BAGIT in submodules/quiver-data/*.zip
-do
-    ocrd zip spill "$BAGIT" -d "$WORKSPACE_DIR" > log.log
-done
-echo "BagIt extraction completed."
+quiver-ocrd || {
+    echo "quiver-ocrd not installed"
+    exit 1
+    }
+oton || {
+    echo "quiver-ocrd not installed"
+    exit 1
+    }
 
 cd "$ROOT" || exit
 
@@ -97,7 +97,6 @@ do
 done
 
 # download the necessary models if not available
-
 echo "Download the necessary models if not available"
 if [[ ! -f $ROOT/models/ocrd-tesserocr-recognize/Fraktur_GT4HistOCR.traineddata ]]
 then
@@ -105,53 +104,61 @@ then
     docker run --volume "$ROOT"/models:/usr/local/share/ocrd-resources -- ocrd/all:maximum \
         ocrd resmgr download ocrd-tesserocr-recognize Fraktur_GT4HistOCR.traineddata
 fi
+if [[ ! -d $ROOT/models/ocrd-calamari-recognize/qurator-gt4histocr-1.0 ]]
+then
+    mkdir -p "$ROOT"/models
+    docker run --volume "$ROOT"/models:/usr/local/share/ocrd-resources -- ocrd/all:maximum ocrd resmgr download ocrd-calamari-recognize qurator-gt4histocr-1.0
+fi
 
 # execute this workflow on the existing data (incl. evaluation)
-
+mkdir -p "$WORKSPACE_DIR"/tmp
 cd "$WORKSPACE_DIR" || exit
+
+# create workspace for all OCR workflows.
+# each workflow has a separate workspace to work with.
+
+
+
+
+echo "Restore OCR-D workspaces from BagIts and create workflow specific workspaces …"
+for BAGIT in "$ROOT"/submodules/quiver-data/*.zip
+do
+    BAGIT_NAME=$(basename -s .ocrd.zip "$BAGIT")
+    ocrd zip spill "$BAGIT" -d "$WORKSPACE_DIR"/tmp > "$WORKSPACE_DIR"/log.log
+    unzip "$BAGIT" METADATA.yml -d "$WORKSPACE_DIR"/tmp/"$BAGIT_NAME"
+
+    for WORKFLOW in "$OCRD_WORKFLOW_DIR"/*ocr.txt.nf
+    do
+        WF_NAME=$(basename -s .txt.nf "$WORKFLOW")
+        cp -r "$WORKSPACE_DIR"/tmp/"$BAGIT_NAME" "$WORKSPACE_DIR"/tmp/"$BAGIT_NAME"_"$WF_NAME"
+        cp "$WORKFLOW" "$WORKSPACE_DIR"/tmp/"$BAGIT_NAME"_"$WF_NAME"/
+    done
+    rm -r "$WORKSPACE_DIR"/tmp/"$BAGIT_NAME"
+done
+
+echo "Clean up intermediate dirs …"
+for DIR in "$WORKSPACE_DIR"/tmp/*
+do
+    DIR_NAME=$(basename "$DIR")
+    mv "$DIR" "$WORKSPACE_DIR"/"$DIR_NAME"
+    cp "$OCRD_WORKFLOW_DIR"/*eval.txt.nf "$WORKSPACE_DIR"/"$DIR_NAME"
+done
+
+rm -rf "$WORKSPACE_DIR"/tmp
+rm "$WORKSPACE_DIR"/log.log
 
 # start webserver for evaluation
 uvicorn api:app --app-dir "$ROOT"/quiver &
 
 # for all data sets…
-for WS_DIR in "$WORKSPACE_DIR"/*/
+for WS_DIR in "$WORKSPACE_DIR"/*
 do
     echo "Switching to $WS_DIR."
-    # … copy all workflows in the right place …
-    for WORKFLOW in "$OCRD_WORKFLOW_DIR"/*.nf
-    do
-        cp "$WORKFLOW" "$WS_DIR"
-    done
 
     DIR_NAME=$(basename $WS_DIR)
 
-    # separate OCR workflows from evaluation workflows
-    NF_FILES=("$WS_DIR"/*.nf)
-    OCR_NFS=()
-    EVAL_NFS=()
-
-    for NF in "${NF_FILES[@]}"
-    do
-        if [[ $NF != *"dinglehopper_eval.txt.nf" ]]; then
-            OCR_NFS+=($NF)
-        else
-            EVAL_NFS+=($NF)
-        fi
-    done
-
-    # run OCR workflows
-    for WORKFLOW in "${OCR_NFS[@]}"
-    do
-        run "$WORKFLOW" "$DIR_NAME"
-        save_workspaces "$WS_DIR" "$DIR_NAME" "$WORKFLOW"
-    done
-
-    # run eval workflows
-    for WORKFLOW in "${EVAL_NFS[@]}"
-    do
-        run "$WORKFLOW" "$DIR_NAME"
-        save_workspaces "$WS_DIR" "$DIR_NAME" "$WORKFLOW"
-    done
+    run "$WS_DIR"/*ocr.txt.nf "$DIR_NAME" "$WS_DIR"
+    run "$WS_DIR"/*eval.txt.nf "$DIR_NAME" "$WS_DIR"
 
     # create a result JSON according to the specs          
     echo "Get Benchmark JSON …"
@@ -159,14 +166,14 @@ do
     echo "Done."
 
     # move data to results dir
-    mv $WS_DIR/*.json "$WORKFLOW_DIR"/results
+    mv "$WS_DIR"/*.json "$WORKFLOW_DIR"/results
 done
 
 cd "$ROOT" || exit
 
 # shut down server
 JOB_NO=$(jobs -l | grep -E -o "[0-9]{4,}")
-kill $JOB_NO
+kill "$JOB_NO"
 
 
 # summarize JSONs
@@ -180,6 +187,6 @@ rm -rf "$ROOT"/work
 rm -rf "$WORKFLOW_DIR"/nf-results
 rm "$WORKFLOW_DIR"/results/*.json
 
-# push results to the quiver-back-end repo
-git add workflows/results/*
+## push results to the quiver-back-end repo
+git add data/workflows.json
 git commit -m "update evaluation results"
